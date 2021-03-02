f94243141c43b2eb2fc2257a40a339d35195b88f
LEEJUNB <topguide82@gmail.com>
2021-03-02 21:56:36 +0900

    html 기본 골격, font awesome 활용

--- introProject/introProject/urls.py
+++ introProject/introProject/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
 from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index), # http://127.0.0.1:8000/
-    path('first/', include('myapp.urls')), # http://127.0.0.1:8000/first
-    path('home/', include('myapp.urls')), # http://127.0.0.1:8000/home
+    path('', views.index), # 주소 : http://127.0.0.1:8000/ | 특징 : index함수 연결
+    # path('', include('myapp.urls')), # 주소 : http://127.0.0.1:8000/ | 특징 : urls 매핑 방식
+    path('home/', include('myapp.urls')), # 주소 : http://127.0.0.1:8000/home | 특징 : myapp앱의 urls와 매핑
 ]
--- introProject/myapp/urls.py
+++ introProject/myapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 from myapp import views
 
 urlpatterns = [
-    path('', views.home),
-    path('home', include('myapp.urls')), # 앱명칭(myapp).urls
+    path('', views.home), # 주소 : http://127.0.0.1:8000/home | 특징 : views파일의 home함수와 연결
 ]
